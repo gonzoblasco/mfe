0d3535f4454eef57198d8937731e3edba61862df
gonzoblasco <gonzoblasco@gmail.com>
2023-11-30 20:48:33 -0300

    Update AWS region setting in GitHub workflow
    The AWS_DEFAULT_REGION configuration has been removed from the .github/workflows/container.yml file and hardcoded as 'us-east-1'. This change decreases the region flexibility but increases workflow consistency by guaranteeing that the execution will always be in the 'us-east-1' AWS region. Also, syntax corrections were made in the file and webpack prod configuration file for better readability and alignment with code styling convention.

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,24 +1,20 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require(
+  'webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN
 
 const prodConfig = {
-  mode: "production",
-  output: {
-    filename: "[name].[contenthash].js",
-  },
-  plugins: [
+  mode: 'production', output: {
+    filename: '[name].[contenthash].js',
+  }, plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
+      name: 'container', remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
-      },
-      shared: packageJson.dependencies,
-    }),
-  ],
-};
+      }, shared: packageJson.dependencies,
+    })],
+}
 
-module.exports = merge(commonConfig, prodConfig);
+module.exports = merge(commonConfig, prodConfig)
